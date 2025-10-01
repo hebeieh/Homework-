afdba222c41f7122a34e3c21037da95640f4a186
hebeieh <dianka.pavlova.06@inbox.ru>
2025-10-02 01:36:48 +0300

    Add files via upload

--- столбчатая диаграмма.py
+++ столбчатая диаграмма.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 x = ['Продукты', 'Транспорт', 'Техника', 'Мобильная связь', 'Культура и искусство', 'Такси']
 y = [17500, 13000, 8500, 1900, 900, 520]
 
-plt.bar(x, y, label='Выписка из банка', alpha=0.1 )
-plt.plot(x, y, color='purple', marker='o', markersize=7)
+colors = plt.cm.viridis(np.linspace(0, 1, len(x)))
+plt.bar(x, y, label='Выписка из банка', alpha=0.2, color=colors)
 plt.xlabel('Категории')
+plt.xticks(rotation=45)
 plt.ylabel('Сумма')
 plt.title('Расходы за месяц')
 plt.legend(loc='upper right')
